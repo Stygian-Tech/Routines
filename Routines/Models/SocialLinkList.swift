a678b768acc0d504de9392bee1c01b4f47ffc932
Sam Clemente <sam@theclementes.com>
2025-02-01 17:27:34 -0600

    Removed Bluesky and Threads links since the accounts are unmonitored, updated Mastodon account to new instance

--- Routines/Models/SocialLinkList.swift
+++ Routines/Models/SocialLinkList.swift
@@ -9,9 +9,9 @@ import Foundation
 
 class SocialLinkList: ObservableObject {
     @Published var symbols: [Symbol] = [
-        .init(name: "Mastodon", file: "Mastodon", url: "https://allthingstech.social/@CountableNewt/", color: .purple),
-        .init(name: "Bluesky", file: "Bluesky", url: "https://bsky.app/profile/samclemente.me", color: .blue),
-        .init(name: "Threads", file: "Threads", url: "https://www.threads.net/@CountableNewt/", color: .primary),
+        .init(name: "Mastodon", file: "Mastodon", url: "https://indieweb.social/@CountableNewt/", color: .purple),
+//        .init(name: "Bluesky", file: "Bluesky", url: "https://bsky.app/profile/samclemente.me", color: .blue),
+//        .init(name: "Threads", file: "Threads", url: "https://www.threads.net/@CountableNewt/", color: .primary),
         .init(name: "Routines on GitHub", file: "GitHub", url: "https://github.com/CountableNewt/Routines", color: .primary)
     ]
 }
